ae1e9d61723f7c13c812eff4c15c631eaa6a0c42
Benjamin Nordin <sockless@protonmail.com>
2020-10-16 15:43:16 +0200

    Add Base Project
    Added the base Unity Project to git, to make it easier to work inbetween computers

---
+++ Assets/Scripts/AnimationBoi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBoi : MonoBehaviour
+{
+    Animator CoverAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoverAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartAnimations()
+    {
+        CoverAnimator.SetBool("IsHover", true);
+    }
+
+    public void StopAnimations()
+    {
+
+        CoverAnimator.SetBool("IsHover", false);
+    }
+}
